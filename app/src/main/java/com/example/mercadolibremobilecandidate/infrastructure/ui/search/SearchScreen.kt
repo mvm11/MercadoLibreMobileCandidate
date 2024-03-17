6067b4c896b9017b8c42c636513f6da318e322fc
mateo <mvalenciaminota@gmail.com>
2024-03-16 22:17:22 -0500

    Testing SearchScreen

--- app/src/main/java/com/example/mercadolibremobilecandidate/infrastructure/ui/search/SearchScreen.kt
+++ app/src/main/java/com/example/mercadolibremobilecandidate/infrastructure/ui/search/SearchScreen.kt
@@ -1,7 +1,6 @@
 package com.example.mercadolibremobilecandidate.infrastructure.ui.search
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,52 +13,43 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.example.mercadolibremobilecandidate.R
 import com.example.mercadolibremobilecandidate.infrastructure.ui.navigation.Routes
 
 
 @Composable
 fun SearchScreen(navController: NavHostController) {
+    var query by remember { mutableStateOf("") }
 
-    Surface(
-        modifier = Modifier
-            .fillMaxSize()
-            .clickable { navController.navigate(Routes.RESULT) }
-    ) {
-        Text(modifier = Modifier
-            .fillMaxWidth()
-            .height(80.dp),
-        text = Routes.SEARCH)
-
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Search(
+            modifier = Modifier.fillMaxSize(),
+            onQueryChange = { query = it },
+            onSearch = { navController.navigate(Routes.RESULT)}
+        )
     }
 }
 
-@Preview
 @Composable
-fun SearchScreenPreview(){
-    SearchScreen(rememberNavController())
-}
-
-@Composable
-fun Search(modifier: Modifier, viewModel: SearchViewModel) {
-
+fun Search(modifier: Modifier, onQueryChange: (String) -> Unit, onSearch: () -> Unit) {
     Column(modifier = modifier) {
         HeaderImage(Modifier.align(Alignment.CenterHorizontally))
         Spacer(modifier = Modifier.padding(16.dp))
-        EmailField("Mateo")
+        QueryField("", onQueryChange)
         Spacer(modifier = Modifier.padding(4.dp))
-        LoginButton()
+        SearchButton(onSearch)
     }
 }
-
 
 @Composable
 fun HeaderImage(modifier: Modifier) {
@@ -71,11 +61,15 @@ fun HeaderImage(modifier: Modifier) {
 }
 
 @Composable
-fun EmailField(email: String) {
+fun QueryField(query: String, onQueryChange: (String) -> Unit) {
+
+    var query: String by remember { mutableStateOf("")}
+
     TextField(
-        value = email, onValueChange = {},
+        value = query,
+        onValueChange = {query = it},
         modifier = Modifier.fillMaxWidth(),
-        placeholder = { Text(text = "Email") },
+        placeholder = { Text(text = "Buscar en Mercado Libre") },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
         singleLine = true,
         maxLines = 1
@@ -83,15 +77,14 @@ fun EmailField(email: String) {
 }
 
 @Composable
-fun LoginButton() {
+fun SearchButton(onClick: () -> Unit) {
     Button(
-        onClick = { },
+        onClick = onClick,
         modifier = Modifier
             .fillMaxWidth()
             .height(48.dp),
         enabled = true
     ) {
-        Text(text = "Iniciar sesión")
+        Text(text = "Buscar")
     }
 }
-
--- app/src/main/java/com/example/mercadolibremobilecandidate/infrastructure/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/mercadolibremobilecandidate/infrastructure/ui/search/SearchViewModel.kt
@@ -1,16 +1,10 @@
 package com.example.mercadolibremobilecandidate.infrastructure.ui.search
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class SearchViewModel: ViewModel() {
 
-    private val _query = MutableLiveData<String>()
-    val query: LiveData<String> = _query
-
-    fun setQuery(input: String) {
-        _query.value = input
-    }
 
 }
+
+
