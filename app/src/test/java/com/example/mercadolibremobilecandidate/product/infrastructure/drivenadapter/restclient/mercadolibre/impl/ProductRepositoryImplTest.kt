79fb56f33e63e637a58fdf2f957f9d9690d8e214
Mateo Valencia Minota <mateo.valencia@GlobalBank.com.pa>
2024-03-18 13:45:25 -0500

    test added

---
+++ app/src/test/java/com/example/mercadolibremobilecandidate/product/infrastructure/drivenadapter/restclient/mercadolibre/impl/ProductRepositoryImplTest.kt
@@ -0,0 +1,46 @@
+package com.example.mercadolibremobilecandidate.product.infrastructure.drivenadapter.restclient.mercadolibre.impl
+
+import com.example.mercadolibremobilecandidate.product.domain.model.Product
+import com.example.mercadolibremobilecandidate.product.infrastructure.drivenadapter.restclient.mercadolibre.api.MercadoLibreAPI
+import com.example.mercadolibremobilecandidate.product.infrastructure.drivenadapter.restclient.mercadolibre.response.ApiResponse
+import com.example.mercadolibremobilecandidate.product.infrastructure.drivenadapter.restclient.mercadolibre.response.Paging
+import org.junit.Assert.*
+
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.`when`
+
+
+class ProductRepositoryImplTest {
+
+    private val mercadoLibreApi = mock(MercadoLibreAPI::class.java)
+    private val productRepository = ProductRepositoryImpl(mercadoLibreApi)
+
+    @Test
+    fun `searchProducts should return expected products list when API call is successful`() = runBlocking {
+        // Arrange
+        val query = "iphone"
+        val expectedProducts = listOf(
+            Product(id = "1", title = "Motorola G6 Plus", price = 17999.0, thumbnail = "http://example.com/image1.jpg"),
+            Product(id = "2", title = "Motorola G6", price = 14899.0, thumbnail = "http://example.com/image2.jpg")
+        )
+        `when`(mercadoLibreApi.searchProducts(query)).thenReturn(
+            ApiResponse(
+                siteId = "MCO",
+                query = query,
+                paging = Paging(total = 2, offset = 0, limit = 50, primaryResults = 2),
+                results = expectedProducts
+            )
+        )
+
+        // Act
+        val actualProducts = productRepository.searchProducts(query)
+
+        // Assert
+        assertEquals(expectedProducts, actualProducts)
+    }
+}
+
